2be0d18802a1c5d74c6003845d467a8e464b6e0a
Ambika Nair <ambiknai@in.ibm.com>
2019-09-22 19:16:44 -0700

    Remove editing etc/hosts file

--- driver/driver.go
+++ driver/driver.go
@@ -19,7 +19,6 @@ import (
 	"github.com/IBM/ibmcloud-object-storage-plugin/utils/parser"
 	"go.uber.org/zap"
 	"io/ioutil"
-	"net/url"
 	"os"
 	"os/exec"
 	"path"
@@ -477,53 +476,6 @@ func (p *S3fsPlugin) mountInternal(mountRequest interfaces.FlexVolumeMountReques
 		}
 		os.Setenv("CURL_CA_BUNDLE", caFile)
 		os.Setenv("AWS_CA_BUNDLE", caFile)
-		u, err := url.Parse(endptValue)
-		if err != nil {
-			p.Logger.Error(podUID+":"+" Unable to parse the endpoint URL",
-				zap.Error(err))
-			return fmt.Errorf("unable to parse the endpoint URL: %v", err)
-		}
-		hostname := u.Hostname()
-		output, err := command("cp", etcHost, etcHostBak).CombinedOutput()
-		if err != nil {
-			p.Logger.Error(podUID+":"+"Unable to create a copy of /etc/hosts file",
-				zap.Error(err))
-			return fmt.Errorf("Unable to create a copy of /etc/hosts file: %s", string(output))
-		}
-		cmd, err := command("grep", "-v", "-w", hostname, etcHostBak).Output()
-		if err != nil {
-			p.Logger.Error(podUID+":"+"Unable to check existing entries for service in /etc/hosts file",
-				zap.Error(err))
-			return fmt.Errorf("Unable to check existing entries for service in /etc/hosts file: %s", string(output))
-		}
-
-		f, err := os.OpenFile(etcHost, os.O_APPEND|os.O_WRONLY, 0600)
-		if err != nil {
-			p.Logger.Error(podUID+":"+" Cannot open etc host file",
-				zap.Error(err))
-			return fmt.Errorf("cannot open etc host file: %v", err)
-		}
-
-		defer f.Close()
-
-		// empty the etc/hosts file
-		f.Truncate(0)
-		f.Seek(0, 0)
-
-		// write contents removing existing service entries
-		if _, err = f.WriteString(string(cmd)); err != nil {
-			p.Logger.Error(podUID+":"+" Cannot clean up etc/hosts file",
-				zap.Error(err))
-			return fmt.Errorf("cannot clean up /etc/hosts file: %v", err)
-		}
-
-		// add new entry for the current service
-		addLine := options.ServiceIP + "     " + hostname + "\n"
-		if _, err = f.WriteString(addLine); err != nil {
-			p.Logger.Error(podUID+":"+" Cannot add entry to etc host file",
-				zap.Error(err))
-			return fmt.Errorf("cannot add entry to /etc/hosts file: %v", err)
-		}
 	}
 	// check that bucket exists before doing the mount
 	err = p.checkBucket(endptValue, regionValue, options.Bucket,
